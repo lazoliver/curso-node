ab168ddc73eaecbbc26d87bfe65f1200d8f4c068
Wilian Silva de Oliveira <wilian.oliveira@credibrf.com.br>
2023-06-06 12:44:39 -0300

    css

---
+++ 7_TEMPLATE_ENGINE/css/index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const port = 3000;
+
+const hbs = exphbs.create({
+  partialsDir: ["views/partials"],
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/dashboard", (req, res) => {
+  const items = [
+    { item: "A", id: 1 },
+    { item: "B", id: 2 },
+    { item: "C", id: 3 },
+  ];
+  res.render("dashboard", { items });
+});
+
+app.get("/post", (req, res) => {
+  const post = {
+    title: "Aprender NodeJS",
+    category: "Javascript",
+    body: "Este artigo vai te ajudar a aprender NodeJS",
+    comments: 4,
+  };
+  res.render("blogpost", { post });
+});
+
+app.get("/blog", (req, res) => {
+  const posts = [
+    {
+      title: "Aprender NodeJS",
+      category: "Javascript",
+      body: "Este artigo vai te ajudar a aprender NodeJS",
+      comments: 4,
+    },
+    {
+      title: "Aprender React",
+      category: "Front End",
+      body: "Este artigo vai te ajudar a aprender React",
+      comments: 2,
+    },
+  ];
+  res.render("blog", { posts });
+});
+
+app.get("/", (req, res) => {
+  const user = {
+    name: "Wilian",
+    surname: "Oliveira",
+    age: 30,
+  };
+
+  const cor = {
+    cor: "Blue",
+  };
+
+  const auth = true;
+
+  const approved = false;
+
+  res.render("home", { user, cor, auth, approved });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta: ${port}`);
+});
