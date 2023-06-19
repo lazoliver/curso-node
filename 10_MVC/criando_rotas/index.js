de144452425576f75e638e6b4436071f3c3469a4
Wilian Silva de Oliveira <lazoliver@Lenovo-YOGA-520-14IKB-2ce312f2.localdomain>
2023-06-19 18:48:22 -0300

    MVC: rotas

---
+++ 10_MVC/criando_rotas/index.js
@@ -0,0 +1,45 @@
+/* external */
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+/* internal */
+const port = 3000;
+const app = express();
+
+/* database */
+const conn = require("./db/conn");
+
+/* models */
+const Task = require("./models/Task");
+
+/* read body */
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+/* handlebars */
+const hbs = exphbs.create({
+  partialsDir: ["views/partials"],
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+/* static */
+app.use(express.static("public"));
+
+conn
+  .sync()
+  .then(async () => {
+    /* server */
+    await app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
