71a648fc7aa575b59927423946d4f5cd54e9bafb
Wilian Silva de Oliveira <lazoliver@Lenovo-YOGA-520-14IKB-2ce312f2.localdomain>
2023-06-26 22:41:20 -0300

    flash messages

--- 10_MVC/projeto_dois/controllers/AuthController.js
+++ 10_MVC/projeto_dois/controllers/AuthController.js
@@ -1,8 +1,22 @@
+const User = require("../models/User");
+
+const bcrypt = require("bcryptjs");
+
 module.exports = class AuthController {
   static async login(req, res) {
     await res.render("auth/login");
   }
   static async register(req, res) {
     await res.render("auth/register");
+  }
+
+  static async registerPort(req, res) {
+    const { name, email, password, confirmpassword } = req.body;
+
+    if (password != confirmpassword) {
+      req.flash("message", "As senhas não são iguais.");
+      res.render("auth/register");
+      return;
+    }
   }
 };
